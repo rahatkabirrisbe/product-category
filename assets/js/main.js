52d3d234333c7f21c897e38a438f797b4a0c7cf9
rahatkabirrisbe <107284570+rahatkabirrisbe@users.noreply.github.com>
2022-06-20 23:13:05 +0600

    update some feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,10 +8,31 @@ formEl.addEventListener('submit', (e) => {
     const inputNameValue = inputNameEl.value;
     const inputPriceValue = inputPriceEl.value;
     
+    // validation;
+    const isValid  = validationInput(inputNameValue, inputPriceValue)
     // show input into UI;
+    if(isValid){
     showInputToUi(inputNameValue, inputPriceValue);
+    } else{
+    alert('your input is not valid')
+    }
+    resetInput(inputNameEl, inputPriceEl)
+
 
 })
+
+function resetInput(name, price){
+    name.value = '';
+    price.value = '';
+}
+
+function validationInput(name, price){
+    if(name.length > 0 && parseFloat(price) > 0){
+        return true;
+    } else {
+        return false;
+    }
+}
 
 function showInputToUi(name, price){
     const liHTMLElm = `<li class="list-group-item collection-item">
