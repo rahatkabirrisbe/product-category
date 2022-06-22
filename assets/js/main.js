9236f68131c2cd2bbdfe602c4b319d154cd540ca
rahatkabirrisbe <107284570+rahatkabirrisbe@users.noreply.github.com>
2022-06-22 22:47:17 +0600

    update local storage feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,8 @@ const  ulElement = document.querySelector('.list-group')
 const searchElm = document.querySelector('#filter')
 
 // Data Layer 
-let data = [];
 
+let data = [];
 // event Listener
 formEl.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -20,14 +20,16 @@ formEl.addEventListener('submit', (e) => {
     // show input into UI;
     if(isValid){
     // data tracking to global
-    assignData(id, inputNameValue, inputPriceValue)
+    const products = createData(id, inputNameValue, inputPriceValue)
+    data.push(products)
+    // data tracking to localStorage
+    addDataToLocalStorage(id, inputNameValue, inputPriceValue)
     // show inputToUi
     showInputToUi(id, inputNameValue, inputPriceValue);
     } else{
     alert('your input is not valid')
     }
     resetInput(inputNameEl, inputPriceEl)
-
 })
 
 ulElement.addEventListener('click', (e) => {
@@ -35,7 +37,12 @@ ulElement.addEventListener('click', (e) => {
     if(e.target.classList.contains('fa-trash')){
         const id = getItemByID(e.target)
         removeDataFromUI(id)
-        removeDataFromTracking(id)
+        removeDataFromTracking(data, id)
+        removeDataFromLocalStorage(id);
+    } 
+
+    if(e.target.classList.contains('fa-edit')){
+        console.log('Hello');
     }
 })
 
@@ -47,7 +54,40 @@ searchElm.addEventListener('keyup', (e)=>{
 
 })
 
+document.addEventListener('DOMContentLoaded', showUIFromLocalStorage)
+
+
 // logical function
+function removeDataFromLocalStorage(id){
+    // if(localStorage.getItem('productList')){
+        const idNum = Number(id.split('-')[1])
+    const data = JSON.parse(localStorage.getItem('productList'))
+    const newData = updateAfterRemove(data, idNum)
+    localStorage.setItem('productList', JSON.stringify(newData))
+// }
+}
+
+function showUIFromLocalStorage(){
+    const data = JSON.parse(localStorage.getItem('productList'))
+    if(localStorage.getItem('productList')){
+        showFilterItemToUI(data)
+    }
+}
+
+function addDataToLocalStorage(id, productName, productPrice){
+    const product = createData(id, productName, productPrice)
+    if(localStorage.getItem('productList')){
+        const products = JSON.parse(localStorage.getItem('productList'))
+        products.push(product)
+    localStorage.setItem('productList', JSON.stringify(products))
+    }else{
+        const products = [];
+        products.push(product)
+        localStorage.setItem('productList', JSON.stringify(products))
+    }
+
+}
+
 function showFilterItemToUI(data){
     ulElement.innerHTML = '';
     data.forEach(product => {
@@ -55,6 +95,7 @@ function showFilterItemToUI(data){
         const liHTMLElm = `<li class="list-group-item collection-item" id="item-${product.id}">
         <strong>${product.productName}</strong>- <span class="price">$${product.productPrice}</span>
         <i class="fa fa-trash float-right"></i>
+        <i class="fas fa-edit float-right" style="margin-right:13px"></i>
         </li>`
         ulElement.insertAdjacentHTML('afterbegin', liHTMLElm)
 
@@ -69,9 +110,13 @@ function removeDataFromUI(id){
     document.querySelector(`#${id}`).remove()
 }
 
-function removeDataFromTracking(id){
+function updateAfterRemove(data, id){
+    return data.filter(product => product.id !== id)
+}
+
+function removeDataFromTracking(data, id){
     const idNum = Number(id.split('-')[1])
-    data = data.filter(product => product.id !== idNum)
+    data = updateAfterRemove(data, idNum)
     
 }
 
@@ -81,13 +126,14 @@ function getItemByID(elm){
 }
 
 
-function assignData(id, productName, productPrice){
+function createData(id, productName, productPrice){
     const productData = {
         id,
         productName,
         productPrice
     }
-    data.push(productData)
+    // data.push(productData)
+    return productData;
 }
 
 function resetInput(name, price){
@@ -107,6 +153,7 @@ function showInputToUi(id, name, price){
     const liHTMLElm = `<li class="list-group-item collection-item" id="item-${id}">
     <strong>${name}</strong>- <span class="price">$${price}</span>
     <i class="fa fa-trash float-right"></i>
+    <i class="fas fa-edit float-right" style="margin-right:13px"></i>
     </li>`
     ulElement.insertAdjacentHTML('afterbegin', liHTMLElm);
 }
