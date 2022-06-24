462ba92228fcb9896cc33ef60068e8d038358dda
rahatkabirrisbe <107284570+rahatkabirrisbe@users.noreply.github.com>
2022-06-24 07:16:31 +0600

    edited feature with localStorage updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,12 @@ const inputNameEl = document.querySelector('.product-name')
 const inputPriceEl = document.querySelector('.product-price')
 const  ulElement = document.querySelector('.list-group')
 const searchElm = document.querySelector('#filter')
+const submitBtnElm = document.querySelector('.add-product')
 
 // Data Layer 
-
 let data = [];
+let updateId;
+
 // event Listener
 formEl.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -14,6 +16,8 @@ formEl.addEventListener('submit', (e) => {
     const inputNameValue = inputNameEl.value;
     const inputPriceValue = inputPriceEl.value;
     const id = data.length + 1;
+    // const createId = idCheck(data,id)
+    // console.log(createId);
 
     // validation;
     const isValid  = validationInput(inputNameValue, inputPriceValue)
@@ -42,7 +46,23 @@ ulElement.addEventListener('click', (e) => {
     } 
 
     if(e.target.classList.contains('fa-edit')){
-        console.log('Hello');
+        // console.log('Hello');
+        // select list by id;
+        const idNum = Number(id.split('-')[1]);
+        updateId = idNum;
+        // console.log(idNum);
+        // find the product;
+        const findProduct = data.find(product => product.id === idNum)
+        console.log(findProduct);
+        // data in input area populate
+        populateUIEditState(findProduct)
+        // update btn
+        if(!document.querySelector('.update-product')){
+            showUpdateBtn()
+        }
+        // data modify 
+        // data store in memory
+        // update & store in localStorage
     }
 })
 
@@ -56,11 +76,60 @@ searchElm.addEventListener('keyup', (e)=>{
 
 document.addEventListener('DOMContentLoaded', showUIFromLocalStorage)
 
+formEl.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('update-product')){
+        // console.log('ddd');
+        const name = inputNameEl.value;
+        const price = inputPriceEl.value;
+        // console.log(name,price)
+        const isError = validationInput(name, price);
+        if(!isError){
+            alert('your edit input not valid')
+            return
+        }else{
+            const editedData = data.map(product => {
+                if(product.id === updateId){
+                    return {
+                        id: updateId,
+                        productName: name,
+                        productPrice: price
+                    }
+                }
+                return product;
+            })
+            submitBtnElm.style.display = 'block'
+            // showInputToUi(editedData.id,editedData.productName,editedData.productPrice)
+            showFilterItemToUI(editedData)
+            resetInput(inputNameEl, inputPriceEl);
+            document.querySelector('.update-product').remove();
+            updateProductToLocalStorage(editedData)
+        }
+    }
+})
+
 
 // logical function
+function updateProductToLocalStorage(editedData){
+    if(localStorage.getItem('productList')){
+        localStorage.setItem('productList', JSON.stringify(editedData))
+    }
+}
+function showUpdateBtn(){
+    const updateBtn = ` <button type="button" class="btn mt-3 btn-block btn-secondary update-product">Update</button>`
+    submitBtnElm.style.display = 'none'
+    formEl.insertAdjacentHTML('beforeend', updateBtn)
+}
+
+function populateUIEditState(product){
+    inputNameEl.value = product.productName;
+    inputPriceEl.value = product.productPrice;
+    // product.productName = inputNameEl.value;
+    // product.productPrice = inputPriceEl.value;
+}
+
 function removeDataFromLocalStorage(id){
     // if(localStorage.getItem('productList')){
-        const idNum = Number(id.split('-')[1])
+    const idNum = Number(id.split('-')[1])
     const data = JSON.parse(localStorage.getItem('productList'))
     const newData = updateAfterRemove(data, idNum)
     localStorage.setItem('productList', JSON.stringify(newData))
@@ -68,9 +137,13 @@ function removeDataFromLocalStorage(id){
 }
 
 function showUIFromLocalStorage(){
-    const data = JSON.parse(localStorage.getItem('productList'))
+    // const data = JSON.parse(localStorage.getItem('productList'))
+
+    // overwriting temp memory data by global scope variable;
+    const localData = JSON.parse(localStorage.getItem('productList'))
     if(localStorage.getItem('productList')){
-        showFilterItemToUI(data)
+        data = localData
+        showFilterItemToUI(localData)
     }
 }
 
@@ -125,6 +198,13 @@ function getItemByID(elm){
     return listItem.getAttribute('id');
 }
 
+function idCheck(data, id){
+    if(data[length - 1].id === id){
+        data[length - 1].id++
+    } else {
+        id
+    }
+}
 
 function createData(id, productName, productPrice){
     const productData = {
